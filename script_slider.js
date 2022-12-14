b9df49583fbe0b6e09591e89d98e4463429a336c
Semen <justpr0z.com.ru@gmail.com>
2022-12-14 22:09:14 +0300

    added styles.css

---
+++ script_slider.js
@@ -0,0 +1,20 @@
+let images = [{
+    url: "https://img.favcars.com/mini/hatch/mini_hatch_2010_wallpapers_14_1280x960.jpg",
+    title: "Mini Cooper черный"
+  }, {
+    url: "https://img.favcars.com/mini/cabrio/mini_cabrio_2009_pictures_5_1280x960.jpg",
+    title: "Mini Cooper красный"
+  }, {
+    url: "https://www.t-r-n.ru/files/modification-images/cb/a8/5c/f9/40061_tmb940.jpg",
+    title: "Mini Cooper синий"
+  }, {
+    url: "https://i.pinimg.com/736x/c5/d9/14/c5d9142556fe74c49a2c1c2d4ea6d46a.jpg",
+    title: "Mini Cooper бордовый"
+  }, {
+    url: "https://auusaca.com/wp-content/uploads/2019/12/6.jpg",
+    title: "Mini Cooper белый"
+}];
+
+function initSlider() {
+    
+}
