bba9e9656e89bba174c5d5448b16b07343395557
Semen <justpr0z.com.ru@gmail.com>
2022-12-15 16:16:10 +0300

    added slider arrows and script-slider

--- script_slider.js
+++ script_slider.js
@@ -10,11 +10,44 @@ let images = [{
   }, {
     url: "https://i.pinimg.com/736x/c5/d9/14/c5d9142556fe74c49a2c1c2d4ea6d46a.jpg",
     title: "Mini Cooper бордовый"
-  }, {
-    url: "https://auusaca.com/wp-content/uploads/2019/12/6.jpg",
-    title: "Mini Cooper белый"
-}];
+  }];
 
 function initSlider() {
-    
+    if (!images || !images.length) return;
+
+    let sliderImages = document.querySelector(".slider__images");
+    let sliderArrows = document.querySelector(".slider__arrows")
+
+    initImages();
+    initArrows();
+
+    function initImages() {
+        images.forEach((image, index) => {
+            let imageDiv = `<div class="image n${index} ${index === 0? "active" : ""}" style="background-image:url(${images[index].url});" data-index="${index}"></div>`;
+            sliderImages.innerHTML += imageDiv; 
+        })
+    }
+
+    function initArrows() {
+        sliderArrows.querySelectorAll(".slider__arrow").forEach(arrow => {
+          arrow.addEventListener("click", function() {
+            let curNumber = +sliderImages.querySelector(".active").dataset.index;
+            let nextNumber;
+            if (arrow.classList.contains("left")) {
+              nextNumber = curNumber === 0? images.length - 1 : curNumber - 1;
+            } else {
+              nextNumber = curNumber === images.length - 1? 0 : curNumber + 1;
+            }
+            moveSlider(nextNumber)
+          });
+        });
+    }
+
+    function moveSlider(num) {
+      sliderImages.querySelector(".active").classList.remove("active");
+      sliderImages.querySelector(".n" + num).classList.add("active");
+    }
 }
+
+
+document.addEventListener("DOMContentLoaded", initSlider);
